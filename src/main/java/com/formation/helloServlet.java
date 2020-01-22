f5f8ed77635d15e732970f807e9dec62fe417df2
unknown <demartinica@hotmail>
2020-01-22 16:07:02 +0100

    modif pour essai jenkins

--- src/main/java/com/formation/helloServlet.java
+++ src/main/java/com/formation/helloServlet.java
@@ -20,7 +20,7 @@ public class helloServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("hello from servlet ");
+		response.getWriter().append("hello from servlet from jenkins ");
 	}
 
 	/**
